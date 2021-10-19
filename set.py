726c50894e87fff0e8fd34ddd08e7a4e48c1356e
ray <92747498+its-not-ray@users.noreply.github.com>
2021-10-19 13:15:12 +0300

    Update set.py

--- set.py
+++ set.py
@@ -1,8 +1,10 @@
 # THIS FILE IS NOT TO BE EXECUTED
-# THIS FILE IS TO BE EDITED ONLY FROM main.py
+# THIS FILE IS TO BE EDITED ONLY FROM main.py        
 
-senders_emails = [""]
-senders_passwords = [""]
+# Does not supports multiple emails (coming soon)
+senders_emails = []
+senders_passwords = []
 
-message_content = [""]
-message_subject = [""]
+# Does not supports multiple messages (coming soon)
+message_content = []
+message_subject = []
